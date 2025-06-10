4cb4c1f8f98e3e0dfa9f4679da628ca6d529f377
kce0508 <kce0508@gmail.com>
2025-06-10 18:07:44 +0900

    코테 하는중

---
+++ 코딩테스트/코딩테스트 완료/MyPortfolioWebApp/Models/Board.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolioWebApp.Models;
+
+public class Board
+{
+    [Key]
+    [Display(Name = "번호")]
+    public int Id { get; set; }
+
+    [Display(Name = "이메일")]
+    [Required(ErrorMessage = "{0}은 필수입니다.")]
+    public string Email { get; set; }
+
+    [DisplayName("작성자")]
+    public string? Writer { get; set; }
+
+    [Display(Name = "제목")]
+    [Required(ErrorMessage = "{0}은 필수입니다.")]
+    public string Title { get; set; }
+
+    [Display(Name = "내용")]
+    [Required(ErrorMessage = "{0}은 필수입니다.")]
+    public string Contents { get; set; }
+
+    [DisplayName("작성일")]
+    [DisplayFormat(DataFormatString = "{0:yyyy년 MM월 dd일}", ApplyFormatInEditMode = true)]
+    [BindNever]
+    public DateTime? PostDate { get; set; }
+
+    [DisplayName("조회수")]
+    [BindNever]
+    public int? ReadCount { get; set; }
+}
